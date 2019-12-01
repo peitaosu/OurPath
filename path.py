57db1a1d4839983c056cc744fbdfbf7bcac734b8
Tony Su <peitaosu@163.com>
2019-12-01 21:42:42 +0800

    draw the map

--- path.py
+++ path.py
@@ -1,2 +1,10 @@
 from gps import *
 from map import *
+from exif import *
+import os, sys
+
+image_path = sys.argv[1]
+exif = get_exif(image_path)
+gps = get_gps(exif)
+map = get_map_gps_info(gps)
+map.draw()
