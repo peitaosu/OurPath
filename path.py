f746fd601cccd030c87b96c0a8576cec936c7e1d
Tony Su <peitaosu@163.com>
2019-12-02 22:00:26 +0800

    draw map only when exif is available

--- path.py
+++ path.py
@@ -5,6 +5,8 @@ import os, sys
 
 image_path = sys.argv[1]
 exif = get_exif(image_path)
-gps = get_gps(exif)
-map = get_map_gps_info(gps)
-map.draw()
+if exif:
+    gps = get_gps(exif)
+    get_map_gps_info(gps).draw()
+else:
+    print("No exif found")
