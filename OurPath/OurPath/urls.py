e5b2da945d21310783f9625a38f4e41c6366467a
Tony Su <peitaosu@163.com>
2020-01-02 21:02:11 +0800

    fix issues

--- OurPath/OurPath/urls.py
+++ OurPath/OurPath/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from views import *
+from . import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^index/', views.index),
+    url(r'^$', views.index),
 ]
