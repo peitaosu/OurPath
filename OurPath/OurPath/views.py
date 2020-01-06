cd8af2d6702009ba353f9b744e6bd1521828375b
Tony Su <peitaosu@163.com>
2020-01-06 23:04:47 +0800

    add map support

--- OurPath/OurPath/views.py
+++ OurPath/OurPath/views.py
@@ -5,7 +5,16 @@ from django.shortcuts import redirect
 import os
 import sys
 import json
+from . import map
 
 def index(request):
     context = {}
     return render(request, 'index.html', context)
+
+def map(request):
+    context = {}
+    latitude = request.GET["latitude"]
+    longitude = request.GET["longitude"]
+    zoom = request.GET["zoom"]
+    map = get_map_from_coordinate(latitude, longitude, zoom)
+    return render(request, 'map.html', context)
