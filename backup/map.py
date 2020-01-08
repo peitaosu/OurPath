f17592ada45cd53d505a6e6de6480252c487a727
Tony Su <peitaosu@163.com>
2020-01-08 22:23:08 +0800

    delete backup folder

--- backup/map.py
+++
@@ -1,8 +0,0 @@
-import gmplot 
-
-def get_map_from_coordinate(latitude, longitude, zoom):
-    return gmplot.GoogleMapPlotter(latitude, longitude, zoom) 
-
-def get_map_from_geocode(geocode):
-    return gmplot.GoogleMapPlotter.from_geocode(geocode)
-
